636bd61d8101a3044a42b98b132842104278bd82
PetarPeychev <petarpeychev98@gmail.com>
2020-02-11 09:52:55 +0000

    fixed some bugs in models.py

--- src/cepf/admin.py
+++ src/cepf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Community_Class, Community, Appointment, Appointment_Feedback, Officer, Engagement_Period
+
+admin.site.register(Community_Class)
+admin.site.register(Community)
+admin.site.register(Appointment)
+admin.site.register(Appointment_Feedback)
+admin.site.register(Officer)
+admin.site.register(Engagement_Period)
--- src/cepf/models.py
+++ src/cepf/models.py
@@ -3,22 +3,27 @@ from django.db import models
 class Community_Class(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
-
-class Community(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
-    community_type = modles.ForeignKey(Community_Class)
-    location = models.CharField(max_length=50)
-    communication_channel = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    community = models.ForeignKey(Community)
-    engagement_period = models.ForeignKey(Engagement_Period)
-    engagement_period_multipler = modles.IntegerField()
+    class Meta:
+        verbose_name_plural = "Community_Classes"
 
 class Engagement_Period(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
+    class Meta:
+        verbose_name_plural = "Engagement_Periods"
+
+class Community(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    community_type = models.ForeignKey(Community_Class, on_delete=models.CASCADE)
+    location = models.CharField(max_length=50)
+    communication_channel = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    engagement_period = models.ForeignKey(Engagement_Period, on_delete=models.CASCADE)
+    engagement_period_multipler = models.IntegerField()
+    class Meta:
+        verbose_name_plural = "Communities"
 
 class Appointment_Feedback(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -26,6 +31,8 @@ class Appointment_Feedback(models.Model):
     start_rating2 = models.FloatField()
     start_rating3 = models.FloatField()
     notes = models.TextField()
+    class Meta:
+        verbose_name_plural = "Appointments_Feedback"
 
 class Officer(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -35,6 +42,6 @@ class Officer(models.Model):
 class Appointment(models.Model):
     id = models.IntegerField(primary_key=True)
     date = models.DateTimeField()
-    appointment_feedback = models.OneToOneField(Appointment_Feedback)
+    appointment_feedback = models.OneToOneField(Appointment_Feedback, on_delete=models.CASCADE)
     officers = models.ManyToManyField(Officer)
-    community = models.ForeignKey(Community)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
