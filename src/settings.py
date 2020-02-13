b80cdb5839a337174acb9f000d1fbcb6c2a28fba
PetarPeychev <petarpeychev98@gmail.com>
2020-02-13 09:40:47 +0000

    added serviceworker and PWA default settings

--- src/settings.py
+++ src/settings.py
@@ -126,3 +126,21 @@ STATICFILES_DIRS = [STATIC_DIR,]
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+# PWA settings
+
+PWA_APP_DEBUG_MODE = True
+
+PWA_APP_NAME = 'CEPF'
+PWA_APP_DESCRIPTION = "Nottinghamshire Community Engagement Peformance Framework"
+PWA_APP_THEME_COLOR = '#03a9f4'
+PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'any'
+PWA_APP_START_URL = '/'
+PWA_APP_ICONS = [ { 'src': '/static/images/my_app_icon.png', 'sizes': '160x160' } ]
+PWA_APP_ICONS_APPLE = [ { 'src': '/static/images/my_apple_icon.png', 'sizes': '160x160' } ]
+PWA_APP_SPLASH_SCREEN = [ { 'src': '/static/images/icons/splash-640x1136.png', 'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)' } ]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
