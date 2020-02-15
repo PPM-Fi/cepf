84ee18e7e5f92fa4b38a77ccd9afcc1633e58bf4
PetarPeychev <petarpeychev98@gmail.com>
2020-02-15 16:18:33 +0000

    added the three urls and empty views - calendar, analytics and communities

--- src/cepf/urls.py
+++ src/cepf/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.calendar, name='calendar'),
+    path('analytics', views.analytics, name='analytics'),
+    path('communities', views.communities, name='communities'),
 ]
--- src/cepf/views.py
+++ src/cepf/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/auth/login/')
-def index(request):
-	template='index.html'
+def calendar(request):
+	template='calendar.html'
+	return render(request, template)
+
+@login_required(login_url='/auth/login/')
+def analytics(request):
+	template='analytics.html'
+	return render(request, template)
+
+@login_required(login_url='/auth/login/')
+def communities(request):
+	template='communities.html'
 	return render(request, template)
