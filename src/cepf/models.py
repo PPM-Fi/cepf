4e2faeed191636da5dd37678996cf0ca05ae593f
PetarPeychev <petarpeychev98@gmail.com>
2020-02-16 00:10:48 +0000

    renamed users table to Officers

--- src/cepf/models.py
+++ src/cepf/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Officer(AbstractUser):
-    pass
+
+    class Meta:
+        db_table = 'Officers'
 
 # class Community_Class(models.Model):
 #     id = models.IntegerField(primary_key=True)
