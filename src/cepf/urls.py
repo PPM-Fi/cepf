75d702f7d8147f06076bb99f7e193cb962b7536a
PetarPeychev <petarpeychev98@gmail.com>
2020-02-16 18:18:32 +0000

    added past assignments view and added logic to fill in calendar from the DB

--- src/cepf/urls.py
+++ src/cepf/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.calendar, name='calendar'),
+    path('history', views.history, name='history'),
     path('analytics', views.analytics, name='analytics'),
     path('communities', views.communities, name='communities'),
 ]
