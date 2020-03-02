994132897c9ea1616b54bd4b9576e4144cc6e8db
SamMWoods <sammichaelwoods@gmail.com>
2020-03-02 16:22:05 +0000

    frontend Add community form

--- src/cepf/forms.py
+++ src/cepf/forms.py
@@ -52,15 +52,14 @@ class AssignForm(forms.Form):
     notes = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}), label='Extra Notes')
 
 class CommunitiesForm(forms.Form):
-    name = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Feedback Notes')
+    name = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Community Name')
 
-    type = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Feedback Notes')
+    type = forms.ChoiceField(widget=forms.widgets.Select(), choices=communities(), label='')
 
-    location = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Feedback Notes')
+    location = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Location')
 
-    communication_channel = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Feedback Notes')
+    communication_channel = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Telephone')
 
-    engagement_period = forms.CharField(widget=forms.Textarea(attrs={'class': 'materialize-textarea'}),label='Feedback Notes')
+    engagement_period = forms.ChoiceField(widget=forms.widgets.Select(), choices=officers(), label='') #choices=officers() need to change to day, week and month
 
-    engagement_period_multipler = forms.IntegerField(widget=forms.NumberInput(attrs={'type':'range', 'step': '1', 'min': '1', 'max': '5'}),
-                                min_value=1, max_value=5, label='')
+    engagement_period_multipler = forms.IntegerField(widget=forms.NumberInput(attrs={}),min_value=1, max_value=31, label='')
